bfb2fd4bc38acede95827e8d08d2bfcdd8949984
PanTomRastr <43680489+PanTomRastr@users.noreply.github.com>
2019-05-20 22:35:15 +0300

    Update and rename sem2/ru/spbu/apmath/prog/hw2/task1.java to sem2/ru/spbu/apmath/prog/hw2/task1/main.java

---
+++ sem2/ru/spbu/apmath/prog/hw2/task1/main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        int enter;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter percentage of your discount");
+        enter = in.nextInt();
+
+        System.out.print(sum.Sum(enter));
+    }
+}
